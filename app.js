6ec9c2921a771d7f9a1842c6cdb5617a92e69718
Paulo Martin <paumar559@gmail.com>
2021-02-24 22:54:02 -0800

    Weird issue with receiving back blank objects

---
+++ app.js
@@ -0,0 +1,80 @@
+//================ Routes =======================
+
+app.get('/location', handleGetLocation);
+app.get('/weather', handleGetWeather);
+app.get('/parks', handleGetParks);
+
+function handleGetLocation(req, res){
+  const sqlCheckingString = 'SELECT * FROM cities WHERE search_query=$1';
+  const sqlCheckingArray = [req.query.city];
+  client.query(sqlCheckingString, sqlCheckingArray)
+    .then(result => {
+      if (result.rows.length > 0){
+        console.log(result);
+        res.send(result.rows[0]);
+      }else {
+        const city = req.query.city;
+        const url = `https://us1.locationiq.com/v1/search.php?key=${LOCATION_API_KEY}&q=${city}&format=json`;
+
+        superagent.get(url).then(stuffThatComesBack => {
+          const output = new Location(stuffThatComesBack.body, city);
+          res.send(output);
+          const sqlString = 'INSERT INTO cities (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4)';
+
+
+          const sqlArray = [city, stuffThatComesBack.body[0].display_name, stuffThatComesBack.body[0].lat, stuffThatComesBack.body[0].lon];
+          client.query(sqlString, sqlArray);
+        }).catch(errorThatComesBack => {
+          console.log(errorThatComesBack);
+          res.status(500).send('something went wrong');
+        });
+      }
+    });
+}
+
+function Location(dataFromTheFile, cityName){
+  this.search_query = cityName;
+  this.formatted_query = dataFromTheFile[0].display_name;
+  this.latitude = dataFromTheFile[0].lat;
+  this.longitude = dataFromTheFile[0].lon;
+}
+
+function handleGetWeather(req, res){
+  const cityName = req.query.search_query;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${WEATHER_API_KEY}`;
+
+  
+  superagent.get(url).then(stuffThatComesBack => {
+    const output = stuffThatComesBack.body.data.map(day => new Weather(day));
+    res.send(output);
+  }).catch(errorThatComesBack => {
+    res.status(500).send(errorThatComesBack);
+  });
+}
+
+function Weather(data){
+  this.forecast = data.weather.description;
+  this.time = data.datetime;
+}
+
+function handleGetParks(req, res){
+  const parkCode = req.query.formatted_query;
+  const url = `https://developer.nps.gov/api/v1/parks?limit=2&start=0&q=${parkCode}&sort=&api_key=${PARKS_API_KEY}`;
+  superagent.get(url).then(stuffThatComesBack => {
+    const output = stuffThatComesBack.body.data.map(park => new Parks(park));
+    res.send(output);
+  }).catch(errorThatComesBack => {
+    res.status(500).send(errorThatComesBack);
+  });
+}
+function Parks(data){
+  this.name = data.fullName;
+  this.address = data.addresses[0].line1;
+  this.fee = data.fees.cost;
+  this.description = data.description;
+  this.url = data.url;
+}
+//=================== Initialization =================
+client.connect().then(() => {
+  app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`));
+});
--- server.js
+++ server.js
@@ -1,35 +1,25 @@
-/*
-PORT=3000
-GEOCODE_API_KEY=pk.d237f2a8cd6e5ca3c0d7f574446126a9	
-PARKS_API_KEY=u0hZ0TLxXnJ1WnBIXc4pBZrrrZ5tAAbtfaZMJn8m
-WEATHER_API_KEY=b32e3a84655e4803a541ef750d4ff318
-DATABASE_URL=post
-
-*/
-
-
-
-
 // ============== Packages ==============================
 
 const express = require('express');
-const cors = require('cors'); // just kinda works and we need it
-// If this line of code comes, delete it const { response } = require('express');
+const cors = require('cors'); 
 const superagent = require('superagent');
-require('dotenv').config(); // read the `.env` file's saved env variables AFTER reading the terminal's real env's variables
+const pg = require('pg');
+require('dotenv').config();
 
 
 // ============== App ===================================
 
-const app = express(); // express() will return a fully ready to run server object
-app.use(cors()); // enables local processes to talk to the server // Cross Origin Resource Sharing
+const app = express();
+app.use(cors()); 
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client(DATABASE_URL);
 
-const PORT = process.env.PORT || 3000; // process.env is boilerplace the variable name is potato
+const PORT = process.env.PORT || 3000;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY
 const WEATHER_API_KEY= process.env.WEATHER_API_KEY
 const PARKS_API_KEY = process.env.PARKS_API_KEY
 
-
+client.on('error', error => console.log(error));
 
 
 // ============== Routes ================================
@@ -37,21 +27,38 @@ const PARKS_API_KEY = process.env.PARKS_API_KEY
 /////////////////// LOCATION INFORMATION ///////////////////
 app.get('/location', handleGetLocation);
 function handleGetLocation(req, res){
-  const city = req.query.city
-  const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`; // Change token Key
-
-  superagent.get(url) 
-    .then(loaction_api_information => {
-    const output = new LocationKit(loaction_api_information.body, city)
-    // console.log(output)
-    res.send(output);  
+ 
+  const sqlCheckingString = 'SELECT * FROM fancykat WHERE search_query=$1';
+  const sqlCheckingArray = [req.query.city];
+  
+  client.query(sqlCheckingString, sqlCheckingArray)
+      .then(result => {
+        // CHhecking row length
+          if (result.rows.length > 0){
+            // console.log(result);
+            res.send(result.rows[0]);
+          } else{
+            const city = req.query.city
+            const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`;
     
+            superagent.get(url) 
+              .then(location_info => {
+              const output = new LocationKit(location_info.body, city)
+              res.send(output);
+              const sqlString = 'INSERT INTO fancykat (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4)';
+              const sqlArray = [city, location_info.body[0].location_info, location_info.body[0].lat, location_info.body[0].lon];
+
+    client.query(sqlString, sqlArray);
 })
 .catch(errorThatComesBack => {
   console.log(errorThatComesBack);
   res.status(500).send('Sorry something went wrong');
-}); 
+})
 }
+})
+}
+
+    
 function LocationKit(dataFromTheFile, cityName){
   this.search_query = cityName;
   this.formatted_query = dataFromTheFile[0].display_name;
@@ -63,20 +70,20 @@ function LocationKit(dataFromTheFile, cityName){
 app.get('/weather', handleGetWeather);
 function handleGetWeather(req, res) {
   const weather = req.query.search_query
-  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${weather}&key=${WEATHER_API_KEY}`; // Change token Key
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${weather}&key=${WEATHER_API_KEY}`; 
 
-  superagent.get(url) 
-  .then(weather_api_information => {
-  const output = weather_api_information.body.data.map(weather_info => new WeatherKit(weather_info))
+  superagent.get(url).then(weather_info => {
+  const output = weather_info.body.data.map(result => new WeatherKit(result))
   // console.log(output)
   res.send(output);
-
 })
 .catch(errorThatComesBack => {
   console.log(errorThatComesBack);
   res.status(500).send('Sorry something went wrong');
 }); 
 }
+
+
 function WeatherKit(object) {
   this.forecast =  object.weather.description;
   this.time = object.valid_date;
@@ -87,12 +94,13 @@ function WeatherKit(object) {
 app.get('/parks', handleGetParks);
 function handleGetParks(req, res) {
   const parkCode = req.query.formatted_query;
-  const url = `https://developer.nps.gov/api/v1/parks?q=${parkCode}&api_key=${PARKS_API_KEY}`; // Change token Key
+  
+  const url =  `https://developer.nps.gov/api/v1/parks?limit=3&start=0&q=${parkCode}&sort=&api_key=${PARKS_API_KEY}`; // Change token Key
 
   superagent.get(url) 
-  .then(result => {
-  const output = result.body.data.map(parkInfo =>  new ParkKit(parkInfo))
-  // console.log(output)
+  .then(parks_info => {
+  const output = parks_info.body.data.map(result => new ParkKit(result))
+  console.log(output)
   res.send(output);
 
 })
@@ -104,16 +112,19 @@ function handleGetParks(req, res) {
 function ParkKit (object){
   this.name = object.fullName;
   this.address = `${object.addresses[0].line1}, ${object.addresses[0].city}, ${object.addresses[0].stateCode}, ${object.addresses[0].postalCode}`;
-  this.fee = object.entranceFees[0].cost;
+  this.fee =  object.entranceFees[0].cost ? object.entranceFees[0].cost: 'No Cost';
   this.description = object.description;
-  this.url = object.url;
+  this.url = object.url
 }
 
 
 
 // ============== Initialization ========================
 
-// I can visit this server at http://localhost:3009
-app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`)); // this is what starts the server
+// I can visit this server at http://localhost:3000
+client.connect().then(() => {
+  app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`));
+});
+
 
 
